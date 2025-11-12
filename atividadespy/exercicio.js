06dd92d1dc1a87a9d2d67797a4f0d0a8cd1fa28e
Ribeirozjki <phassumpcao246@gmail.com>
2025-11-12 09:23:53 -0300

    Add files via upload

---
+++ atividadespy/exercicio.js
@@ -0,0 +1,115 @@
+// // // // //1)
+// // // // let A = 10;
+// // // // let B = 20;
+
+
+// // // // document.write("A:", A, "B:", B);
+// // // // alert("A:" + A + " B:" + B);  
+// // // // document.write("A:" + A + " B:" + B);  
+
+// // // //02)
+// // // // function calcularSalario() {
+// // // //     let gratificacao = salarioBase * 0.05; 
+// // // //     let salario_gratificacao = salarioBase + gratificacao;
+// // // //     let imposto = salario_gratificacao * 0.07;  
+// // // //     let salarioFinal = salario_gratificacao - imposto;
+// // // //     return salarioFinal;
+// // // // }
+
+
+// // // // let salarioBase = 2000;  
+// // // // let salarioFinal = calcularSalario();
+// // // // document.write("Salário final:" + salarioFinal);
+
+// // // //03)
+// // // // let nome = prompt("Qual é o seu nome?");
+// // // // let idade = prompt("Quantos anos você tem?");
+
+// // // // document.write("Nome:" + nome + ", Idade:" + idade);
+// // // // alert("Nome:" + nome + ", Idade:" + idade);
+// // // // document.write("Nome:" + nome + "Idade:" + idade);
+
+// // // 04)
+// // // let x = parseInt(prompt("Digite o primeiro número:"));
+// // // let y = parseInt(prompt("Digite o segundo número:"));
+
+// // // document.write("Soma: " + (x + y));
+// // // document.write("Multiplicação: " + (x * y));
+// // // document.write("Divisão: " + (x / y));
+// // // document.write("Subtração: " + (x - y));
+
+// // //05)
+// // function soma(x, y) {
+// //     return x + y;
+// // }
+
+// // function multiplicacao(x, y) {
+// //     return x * y;
+// // }
+
+// // function divisao(x, y) {
+// //     return x / y;
+// // }
+
+// // function subtracao(x, y) {
+// //     return x - y;
+// // }
+
+// // let x = parseInt(prompt("Digite o primeiro número:"));
+// // let y = parseInt(prompt("Digite o segundo número:"));
+
+// // document.write("Soma: " + soma(x, y));
+// // document.write("Multiplicação: " + multiplicacao(x, y));
+// // document.write("Divisão: " + divisao(x, y));
+// // document.write("Subtração: " + subtracao(x, y));
+
+// //06)
+// // let nome = prompt("Qual é o seu nome?");
+// // let endereco = prompt("Qual é o seu endereço (com cidade, estado e CEP)?");
+// // let telefone = prompt("Qual é o seu número de telefone?");
+// // let areaAtuacao = prompt("Qual é a sua área de atuação profissional?");
+
+// // document.write("Nome: " + nome);
+// // document.write("Endereço: " + endereco);
+// // document.write("Telefone: " + telefone);
+// // document.write("Área de Atuação: " + areaAtuacao);
+
+// //07)
+// // let totalVendas = parseFloat(prompt("Digite o valor total das vendas projetadas:"));
+// // let lucro = totalVendas * 0.23; 
+// // document.write("Lucro projetado: " + lucro);
+// // alert("Lucro projetado: " + lucro);
+// // document.write("Lucro projetado: " + lucro);
+
+// //08
+// // let num1 = parseInt(prompt("Digite o primeiro número:"));
+// // let num2 = parseInt(prompt("Digite o segundo número:"));
+// // let num3 = parseInt(prompt("Digite o terceiro número:"));
+// // let num4 = parseInt(prompt("Digite o quarto número:"));
+// // let num5 = parseInt(prompt("Digite o quinto número:"));
+
+// // let maior = Math.max(num1, num2, num3, num4, num5);
+
+// // document.write("O maior número é: " + maior);
+
+// //09)
+// // let numPessoas = parseInt(prompt("Quantas idades você vai informar?"));
+// // let maiorDeIdade = 0;
+// // let menorDeIdade = 0;
+
+// // for (let i = 0; i < numPessoas; i++) {
+// //     let idade = parseInt(prompt("Digite a idade da pessoa " + (i+1) + ":"));
+// //     if (idade >= 18) {
+// //         maiorDeIdade++;
+// //     } else {
+// //         menorDeIdade++;
+// //     }
+// // }
+
+// // document.write("Maior de idade: " + maiorDeIdade);
+// // document.write("Menor de idade: " + menorDeIdade);
+
+
+
+
+

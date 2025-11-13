50ffb9bc3e2471a7d12527c1387143c0b19183bf
Ribeirozjki <phassumpcao246@gmail.com>
2025-11-13 17:59:41 -0300

    Update exercicio2.js

--- atividadespy/exercicio2.js
+++ atividadespy/exercicio2.js
@@ -1,66 +1,86 @@
-// //01)
-// // function mercado() {
-// // var fruta = prompt("Escreva A fruta Desejada: ").toLowerCase();
+// // // //01)
+// // // // function mercado() {
+// // // // var fruta = prompt("Escreva A fruta Desejada: ").toLowerCase();
 
-// // switch (fruta) {
-// //     case "maçã":
-// //         alert("Não Vendemos Essa Fruta Aqui")
-// //     break;
-// //     case "kiwi":
-// //         alert("Estamos Com Escassez de Kiwis")
-// //     break;
-// //     case "melancia":
-// //         alert("Aqui,são Três Reais o Quilo")
-// //     break;
-// //     default:
-// //         alert("ERRO, NÃO VENDEMOS ESSE TIPO DE FRUTA AQUI")
-// //         break;
-// // }
-
-
-
-// // }
-// // mercado();
-
-// //02)
-// // function revenda(){
-// // let carro = prompt("Qual Modelo De Carro Irá Querer: ").toLowerCase();
+// // // // switch (fruta) {
+// // // //     case "maçã":
+// // // //         alert("Não Vendemos Essa Fruta Aqui")
+// // // //     break;
+// // // //     case "kiwi":
+// // // //         alert("Estamos Com Escassez de Kiwis")
+// // // //     break;
+// // // //     case "melancia":
+// // // //         alert("Aqui,são Três Reais o Quilo")
+// // // //     break;
+// // // //     default:
+// // // //         alert("ERRO, NÃO VENDEMOS ESSE TIPO DE FRUTA AQUI")
+// // // //         break;
+// // // // }
 
 
-// //     switch (carro) {
-// //         case "hatch":
-// //             alert("Compra efetuada com sucesso.")
-// //         break;
-// //         case "sedan":
-// //             alert("Tem certeza que não prefere este modelo?")
-// //         break;
-// //         case "motocicleta":
-// //             alert("Tem certeza que não prefere este modelo?")
-// //         break;
-// //         case "caminhão":
-// //             alert("Tem certeza que não prefere este modelo?.")
-// //         default:
-// //             alert("NÃO TRABALHAMOS COM ESSE TIPO DE AUTOMÓVEL AQUI")
-// //         break;
-// //     }
 
-// // }
-// // revenda();
+// // // // }
+// // // // mercado();
 
-// //03)
+// // // //02)
+// // // // function revenda(){
+// // // // let carro = prompt("Qual Modelo De Carro Irá Querer: ").toLowerCase();
+
+
+// // // //     switch (carro) {
+// // // //         case "hatch":
+// // // //             alert("Compra efetuada com sucesso.")
+// // // //         break;
+// // // //         case "sedan":
+// // // //             alert("Tem certeza que não prefere este modelo?")
+// // // //         break;
+// // // //         case "motocicleta":
+// // // //             alert("Tem certeza que não prefere este modelo?")
+// // // //         break;
+// // // //         case "caminhão":
+// // // //             alert("Tem certeza que não prefere este modelo?.")
+// // // //         default:
+// // // //             alert("NÃO TRABALHAMOS COM ESSE TIPO DE AUTOMÓVEL AQUI")
+// // // //         break;
+// // // //     }
+
+// // // // }
+// // // // revenda();
+
+// // // 03)
 // // function validacao(){
-// //  let idade = parseInt(prompt("Escreva sua idade: "));
+// //     let verdadeiro = true;
 
-// //  let resultado = (idade >= 18) ? "Maior de Idade" : "Menor de Idade";
-// //  alert(resultado);
+// //     let resultado = verdadeiro ? 'true' : 'false';
+// //     alert(`Ele é ${resultado} `);
 // // }
 // // validacao();
 
-// //06)
-// function saudacao(nome, sobrenome) {
-//   console.log("Olá " + nome + " " + sobrenome + ", eu sou uma função!");
+// // // //04)
+// // function comparar(){
+// //  let programador = false;
+
+// //  let resultado = programador ? 'É programador' : 'Não é Programador';
+
+// //  alert(resultado);
+
+// // }
+// // comparar();
+
+// //05)
+// function validarDados(){
+// let num1 = parseInt(prompt("Escreva o primeiro número: "))
+// let num2 = parseInt(prompt("Escreva o segundo número: "))
+
+// let resultado = (num1 > num2) ? 'O primeiro número é maior' : 'O segundo número é maior'
+// alert(resultado)
 // }
+// validarDados();
 
-// saudacao("Pedro", "Silva");
+// // // //06)
+// // // function saudacao(nome, sobrenome) {
+// // //   console.log("Olá " + nome + " " + sobrenome + ", eu sou uma função!");
+// // // }
 
+// // // saudacao("Pedro", "Silva");
 
